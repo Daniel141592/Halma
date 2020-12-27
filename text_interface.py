8658f5e8d051753b9dc14b59291f929f8f6378c8
Daniel <danielkobialka23@gmail.com>
2020-12-27 21:45:09 +0100

    basic text interface added

---
+++ text_interface.py
@@ -0,0 +1,49 @@
+from game_engine import GameEngine
+from incorrect_move_exception import IncorrectMoveException
+from constants import NUM_OF_PLAYERS
+
+
+def main():
+    player_names = []
+    for _ in range(NUM_OF_PLAYERS):
+        print("Podaj nazwę gracza:")
+        name = input()
+        player_names.append(name.strip())
+
+    game = GameEngine(player_names)
+
+    while game.get_winner() is None:
+        display_board(game.get_board())
+        print(f"Kolej gracza {game.get_now_turn().get_name()}")
+        try:
+            old_position, new_position = parse_input(input())
+            game.make_move(old_position, new_position)
+        except ValueError:
+            print("Incorrect input")
+        except IncorrectMoveException as e:
+            print(e.message)
+
+    display_board(game.get_board())
+    print(f"Wygrał gracz {game.get_winner()}")
+
+
+def parse_input(move):
+    old_x = int(move[0:move.index(',')].strip())
+    old_y = int(move[move.index(',')+1:move.index('->')].strip())
+
+    new_x = int(move[move.index('->')+2:move.rindex(',')].strip())
+    new_y = int(move[move.rindex(',')+1:])
+
+    return ((old_x, old_y), (new_x, new_y))
+
+
+def display_board(board):
+    for row in board:
+        row_str = ""
+        for sign in row:
+            row_str += f"{sign} "
+        print(row_str)
+
+
+if __name__ == "__main__":
+    main()
